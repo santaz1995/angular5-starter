760569c42db0b15842d83cb0cc777043f0e7a390
Vyacheslav <janjo@live.ru>
2018-02-21 17:22:37 +0200

    Finish layout

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 
 @Component({
   templateUrl: 'index.component.html',
 })
-export class IndexComponent { }
+export class IndexComponent {
+
+  public config: SwiperConfigInterface = {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    observer: true,
+    keyboard: true,
+    mousewheel: false,
+    scrollbar: false,
+    navigation: true,
+    pagination: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  };
+
+
+}
--- src/app/index/index.module.ts
+++ src/app/index/index.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from 'app/index/index.component';
 import { IndexRoutingModule } from 'app/index/index-routing.module';
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto'
+};
 
 @NgModule({
   imports: [
     CommonModule,
+    SwiperModule,
     IndexRoutingModule,
   ],
   providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
   ],
   declarations: [ IndexComponent ]
 })
