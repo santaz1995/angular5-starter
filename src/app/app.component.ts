46b0f7910329fa37b58b108de4b2ca094ada5ea9
Vyacheslav <janjo@live.ru>
2018-02-27 11:37:28 +0200

    Fix layout open and close. Add routerLinkActive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 
 @Component({
+  // tslint:disable-next-line
   selector: 'body',
   templateUrl: 'app.component.html',
 })
-export class AppComponent {
-
-}
+export class AppComponent { }
